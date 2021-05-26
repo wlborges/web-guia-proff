725255a313b36ef26cc1afc7e011dff28cc99815
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-05-26 13:48:05 -0300

    create header and footer chat

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,19 +1,38 @@
+import { useState } from 'react'
 import RobotImg from '../../assets/images/robot-img.svg'
+import RobotHead from '../../assets/images/robot-head.svg'
+import { IoMdSend } from 'react-icons/io'
+
 import './styles.css'
 const Chat = () => {
-    return(
+    const [session, setSession] = useState(true)
+    return (
         <div id="chat">
-           <div className="session-off">
-                <img src={RobotImg} alt="Iniciar Chat" />
-                <span>PRENCHA O FORMULARIO PARA INICIAR O CHAT</span>
-           </div>
-           <div className="session-on">
-                <header></header>
-                <body>
-                    
-                </body>
-                <footer></footer>
-           </div>
+            {
+                !session &&
+                <div className="session-off">
+                    <img src={RobotImg} alt="Iniciar Chat" />
+                    <span>PRENCHA O FORMULARIO PARA INICIAR O CHAT</span>
+                </div>
+            }
+            {
+                session &&
+                <div className="session-on">
+                    <header>
+                        <label>
+                            <img src={RobotHead} alt="GABY" />
+                        </label>
+                        <span>GABY</span>
+                    </header>
+                    <body>
+                        <input type="text" placeholder="Digite sua mensagem" />
+                        <button>
+                            <IoMdSend />
+                        </button>
+                    </body>
+                    <footer></footer>
+                </div>
+            }
         </div>
     )
 }
