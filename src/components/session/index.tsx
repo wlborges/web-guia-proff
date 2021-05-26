d160de1ca18302c06ba1fc3f529296325ed6a3ea
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-05-26 00:52:56 -0300

    create session on content

--- src/components/session/index.tsx
+++ src/components/session/index.tsx
@@ -1,21 +1,40 @@
-import {FiUser, FiMail} from 'react-icons/fi'
+import { FormEvent, useState } from 'react'
+import { FiUser, FiMail } from 'react-icons/fi'
+import RobotHead from '../../assets/images/robot-head.svg'
 
 import './styles.css'
 const Session = () => {
-    return(
+    const [session, setSession] = useState(false)
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+        setSession(!session)
+    }
+
+    return (
         <div id="session">
-            <form>
-                <span>Iniciar Chat</span>
-                <div className="input-content">
-                    <FiUser/>
-                    <input type="text" placeholder="Digite seu nome" />
+            {
+                !session &&
+                <form onSubmit={handleSubmit}>
+                    <span>Iniciar Chat</span>
+                    <div className="input-content">
+                        <FiUser />
+                        <input type="text" placeholder="Digite seu nome" />
+                    </div>
+                    <div className="input-content">
+                        <FiMail />
+                        <input type="email" placeholder="Digite seu email" />
+                    </div>
+                    <button type="submit">INICIAR</button>
+                </form>
+            }
+            {
+                session &&
+                <div className="session-on">
+                    <img src={RobotHead} alt="Session On" />
+                    <h1>Você esta conversando com a <span>GABY</span></h1>
                 </div>
-                <div className="input-content">
-                    <FiMail/>
-                    <input type="email" placeholder="Digite seu email" />
-                </div>
-                <button>INICIAR</button>
-            </form>
+            }
         </div>
     )
 }
