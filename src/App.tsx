cf92d199d335e807f541edb609a2eab2d1b584e9
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-05-26 16:32:21 -0300

    add context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Home from './page/home'
+import SessionContext from './contexts/session'
 
 import './assets/styles/global.css'
-function App() {
+import { useState } from 'react';
+function App(): JSX.Element {
+  const [session, setSession] = useState(false)
+
   return (
-    <>
+    <SessionContext.Provider value={{session, setSession}}>
     <Home/>
-    </>
+    </SessionContext.Provider>
   );
 }
 
