6510359181f77f4f4c319462b05b0f2c0d5638ce
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-05-27 18:16:59 -0300

    responsivle path

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,15 +1,19 @@
+import { useContext } from "react"
 import Chat from "../../components/chat"
 import Session from "../../components/session"
 import Sidebar from "../../components/sidebar"
+import SessionContext from '../../contexts/session'
 
 import './styles.css'
 const Home = () => {
+    const { session } = useContext(SessionContext)
+
     return (
         <div id="home-page">
             <div className="sidebar-content">
                 <Sidebar />
             </div>
-            <div className="session-content">
+            <div className={session ? "active session-content" : "session-content"}>
                 <Session />
             </div>
             <div className="chat-content">
