59cf2a260c2d90573413d55be78124f00d2375b9
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2021-05-27 23:05:04 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://guiaproff.herokuapp.com',
+  baseURL: 'https://guiaproff.herokuapp.com',
   headers: {
     'Content-Type': ' application/json',
   }
